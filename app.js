dde0f4b53ea48b7fc263b823f700dff0562c9e3d
Vidit Awasthi <viditawasthi2@gmail.com>
2021-03-13 23:45:45 +0530

    code changes

--- app.js
+++ app.js
@@ -35,18 +35,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
